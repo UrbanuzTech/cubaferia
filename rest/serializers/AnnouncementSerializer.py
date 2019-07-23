12184c1528197357bac7ffa5a86322db0d9e3d62
jose <jaalvarez@uci.cu>
2019-07-23 01:18:35 -0400

    - api select filtered.

--- rest/serializers/AnnouncementSerializer.py
+++ rest/serializers/AnnouncementSerializer.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 
-from rest.models import Announcement
+from rest.models import Announcement, Nomenclature
+from rest.models.nomenclature import CITY, ANNOUNCEMENT_CATEGORY
+
+
+class CategoryField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value
+
+
+class CityField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value
 
 
 class AnnouncementSerializer(serializers.ModelSerializer):
+    category = CategoryField(queryset=Nomenclature.get_by_type(ANNOUNCEMENT_CATEGORY))
+    city = CityField(queryset=Nomenclature.get_by_type(CITY))
+
     class Meta:
         model = Announcement
-        fields = ['id', 'title', 'description', 'visit_count', 'created_at', 'published_at', 'phones', 'emails',
+        fields = ['id', 'title', 'description', 'published_at', 'phones', 'emails',
                   'contact_name', 'address', 'main_image', 'image1', 'image2', 'image3', 'price', 'category', 'city',
                   'created_by']
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'visit_count', 'created_at']
--- rest/serializers/EventSerializer.py
+++ rest/serializers/EventSerializer.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 
+from rest.models import Nomenclature
 from rest.models.announcement import Event
+from rest.models.nomenclature import EVENT_CATEGORY, CITY
+
+
+class CategoryField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value
+
+
+class CityField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value
 
 
 class EventSerializer(serializers.ModelSerializer):
+    category = CategoryField(queryset=Nomenclature.get_by_type(EVENT_CATEGORY))
+    city = CityField(queryset=Nomenclature.get_by_type(CITY))
+
     class Meta:
         model = Event
         fields = ['id', 'title', 'description', 'visit_count', 'created_at', 'published_at', 'phones', 'emails',
                   'contact_name', 'address', 'main_image', 'image1', 'image2', 'image3', 'start_date', 'end_date',
                   'allow_children', 'price_for_children', 'price_for_adults', 'category', 'city', 'created_by']
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'visit_count', 'created_at']
