954e2e464f9cfb48175f85981d3236bb322718c6
jose <jaalvarez@uci.cu>
2019-07-30 14:36:35 -0400

    - list per page 20 - cubaferia.xml - add jquery to project.

--- cubaferia/settings.py
+++ cubaferia/settings.py
@@ -98,7 +98,9 @@ USE_L10N = True
 USE_TZ = False
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
