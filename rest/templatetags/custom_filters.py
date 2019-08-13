70b4061e3815c16790c6b144577101fa1d986d81
jose <jaalvarez@uci.cu>
2019-08-13 13:45:02 -0400

    - adminlte to Home Page.

---
+++ rest/templatetags/custom_filters.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import Group, User
+from django.template import Library
+
+from rest.models import Announcement, Nomenclature
+from rest.models.announcement import Event
+
+register = Library()
+
+
+@register.filter(name='model_info')
+def model_info(model):
+    icons = {
+        'Group': 'users',
+        'User': 'user',
+        'Announcement': 'list-alt',
+        'Event': 'list-alt',
+        'Nomenclature': 'cog'
+    }
+    colors = {
+        'Group': 'blue',
+        'User': 'red',
+        'Announcement': 'green',
+        'Event': 'yellow',
+        'Nomenclature': 'purple'
+    }
+    counts = {
+        'Group': Group.objects.count(),
+        'User': User.objects.count(),
+        'Announcement': Announcement.objects.count(),
+        'Event': Event.objects.count(),
+        'Nomenclature': Nomenclature.objects.count()
+    }
+    return {
+        'icon': icons[model['object_name']] if model['object_name'] in icons else 'circle-o',
+        'color': colors[model['object_name']] if model['object_name'] in colors else 'blue',
+        'count': counts[model['object_name']] if model['object_name'] in counts else '0',
+    }
