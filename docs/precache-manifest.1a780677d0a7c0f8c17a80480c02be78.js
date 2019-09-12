4f2a3d01ab112b6a3212f95f69b13c0f5eb59f70
Yoel <yoediaz92@gmail.com>
2019-09-12 19:59:50 -0400

    - build deploy

---
+++ docs/precache-manifest.1a780677d0a7c0f8c17a80480c02be78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a766d5a9899f3900dd572aa118b370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae849ed142ae9c4574d6",
+    "url": "/static/css/main.986bf13c.chunk.css"
+  },
+  {
+    "revision": "020ec58970562cbad3da",
+    "url": "/static/js/2.b9295be1.chunk.js"
+  },
+  {
+    "revision": "ae849ed142ae9c4574d6",
+    "url": "/static/js/main.4c14b769.chunk.js"
+  },
+  {
+    "revision": "097ccdfd4916e126ca5f",
+    "url": "/static/js/runtime~main.d1a42eb6.js"
+  }
+]);
