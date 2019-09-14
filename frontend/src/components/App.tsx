194e26c6107f96b343d08d8680086442856397c8
Yoel <yoediaz92@gmail.com>
2019-09-14 16:27:37 -0400

    - sidebar & font-awesome added

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -3,31 +3,77 @@ import './App.css';
 import Footer from './generic/footer'
 import Header from "./generic/header";
 import NavMenu from "./generic/nav_menu";
+import {pushRotate as Menu} from 'react-burger-menu'
+// import * as provider from './utils/provider';
+// import { render } from 'react-dom';
 
-const App: React.FC = () => {
-    return (
-        <div className="App">
+class App extends React.Component {
+    // provider.getValueList('nomenclature').then((data) => {
+    //     console.log(data);
+    // }, (err) => {
+    //     console.log(err);
+    // });
 
-            <Header/>
+    showSettings(e: any) {
+        e.preventDefault();
+    }
 
-            <header className="App-header">
-                <p>
-                    CubaFeria
-                </p>
-                <a className="App-link"
-                   href="https://github.com/UrbanuzTech/cubaferia"
-                   target="_blank"
-                   rel="noopener noreferrer">
-                    GitHub
-                </a>
-            </header>
+    render() {
+        return (
 
-            <NavMenu/>
 
-            <Footer/>
+            <div className="App">
 
-        </div>
-    );
+                <div id="outer-container">
+
+                    <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"} disableAutoFocus>
+                        <div className="Card">
+                            <p className="Text">CubaFeria</p>
+                            <a className="App-link"
+                               href="https://github.com/UrbanuzTech/cubaferia"
+                               target="_blank"
+                               rel="noopener noreferrer">
+                                <i className="fa fa-github"> </i><br/>
+                                GitHub
+                            </a>
+                        </div>
+                        <a id="home" className="bm-item" href="/">Continuar Facebook <i
+                            className="fa fa-facebook-square"> </i></a>
+                        <a id="about" className="bm-item" href="/">Continuar Google <i
+                            className="fa fa-google"> </i></a>
+                        <a id="contact" className="bm-item" href="/">Regístrate Gratis</a>
+                    </Menu>
+
+                    <main id="page-wrap">
+
+                        <Header/>
+
+                        <header className="App-header">
+                            <div className="Card">
+                                <div className="Text">
+                                    <p>CubaFeria</p>
+                                    <a className="App-link"
+                                       href="https://github.com/UrbanuzTech/cubaferia"
+                                       target="_blank"
+                                       rel="noopener noreferrer">
+                                        <i className="fa fa-github"> </i><br/>
+                                        GitHub
+                                    </a>
+                                </div>
+                            </div>
+                        </header>
+
+                        <NavMenu/>
+
+                        <Footer/>
+
+                    </main>
+
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
