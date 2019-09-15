6d75980bc0144e4bafdfaf31575ab02f277c41de
Yoel <yoediaz92@gmail.com>
2019-09-14 21:30:27 -0400

    - minor fixes

---
+++ docs/precache-manifest.09d7b9d146989b91286504b909d4397a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589d6f3b2c3b4878be7e5d22208de50f",
+    "url": "/cubaferia/index.html"
+  },
+  {
+    "revision": "d1c1ce2f9a72fc2a8f64",
+    "url": "/cubaferia/static/css/main.986bf13c.chunk.css"
+  },
+  {
+    "revision": "020ec58970562cbad3da",
+    "url": "/cubaferia/static/js/2.b9295be1.chunk.js"
+  },
+  {
+    "revision": "d1c1ce2f9a72fc2a8f64",
+    "url": "/cubaferia/static/js/main.9dc290f6.chunk.js"
+  },
+  {
+    "revision": "c3665c1fa7decac40b10",
+    "url": "/cubaferia/static/js/runtime~main.40abe5b9.js"
+  }
+]);
